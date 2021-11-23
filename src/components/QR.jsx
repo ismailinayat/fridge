296c884bc5624124fb5cdee0acef0a27b2fef448
ismailinayat <ismailinayat@gmail.com>
2021-11-23 22:32:28 +0500

    Replaced placeholder qr code with real qr code that returns the url to the welcome screen when scanned

--- src/components/QR.jsx
+++ src/components/QR.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import QRCode from 'qrcode.react';
 
 function QR() {
     return (
         <div className='qr_scanner'>
-            <Link to='/welcome'>
-                <img src="/qr.png" alt="qr code" />
-            
-            </Link>
+            <QRCode value="https://fridge-sigma.vercel.app/welcome" size={80}></QRCode>
         </div>
     )
 }
 
 export default QR
+/*
+<div className='qr_scanner'>
+            <Link to='/welcome'>
+                <img src="/qr.png" alt="qr code" />
+            
+            </Link>
+        </div>*/
