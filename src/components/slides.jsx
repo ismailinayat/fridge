3c309c9b08a7b3dc150fe9c217fdbb29f1eeb3aa
ismailinayat <ismailinayat@gmail.com>
2021-11-23 16:21:23 +0500

    completed shopping UI page

--- src/components/slides.jsx
+++ src/components/slides.jsx
@@ -1,11 +1,9 @@
-import {useState} from 'react'
+
 import Slide from './slide1'
-import Slide2 from './slide2'
-import Slide3 from './slide3'
+
 
 function Slides() {
 
-    const [numSlides, setNumSlides] = useState(3)
     return (
         <div className="slides__container">
             <Slide></Slide>
