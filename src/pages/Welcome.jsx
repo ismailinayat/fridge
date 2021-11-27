b9400ad575f6662617d8322226ae9a6134443183
ismailinayat <ismailinayat@gmail.com>
2021-11-27 05:11:28 +0500

    fix bugs

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -17,6 +17,7 @@ export default function Welcome(props) {
 
   useEffect(() => {
     if (socket == null) return
+    console.log('welcome emit triggered')
     socket.emit('welcome')
   },[socket])
 
